b2b3de4aaefe812a934a3d7e4928d23e509a0fcd
KonstantinAMP <gijoe403@gmail.com>
2016-04-09 16:26:01 +0400

    Initial commit

---
+++ src/main/java/org/mycompany/entity/dao/impl/StudentDaoImpl.java
@@ -0,0 +1,96 @@
+package org.mycompany.entity.dao.impl;
+
+import org.mycompany.entity.mapper.StudentMapper;
+import org.mycompany.entity.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class StudentDaoImpl
+{
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplateObject;
+
+    public void setDataSource(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+        this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+    }
+
+    public void create(final String name, final Integer age)
+    {
+        final String SQL = "insert into Student (name, age) values (?, ?)";
+
+        jdbcTemplateObject.update(new PreparedStatementCreator()
+        {
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException
+            {
+                PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+                preparedStatement.setString(1, name);
+                preparedStatement.setInt(2, age);
+
+                return preparedStatement;
+            }
+        });
+        System.out.println("Created Record Name = " + name + " Age = " + age);
+        return;
+    }
+
+    public Student getStudent(Integer id)
+    {
+        String SQL = "select * from Student where id = ?";
+        Student student = (Student) jdbcTemplateObject.queryForObject(SQL,
+                new Object[]{id}, new StudentMapper());
+        return student;
+    }
+
+    public List<Student> listStudents()
+    {
+        String SQL = "select * from Student";
+        List<Student> students = jdbcTemplateObject.query(SQL,
+                new StudentMapper());
+        return students;
+    }
+
+    public void delete(final Integer id)
+    {
+        final String SQL = "delete from Student where id = ?";
+        jdbcTemplateObject.update(new PreparedStatementCreator()
+        {
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException
+            {
+                PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+                preparedStatement.setInt(1, id);
+
+                return preparedStatement;
+            }
+        });
+        System.out.println("Deleted Record with ID = " + id);
+        return;
+    }
+
+    public void update(final Integer id, final Integer age)
+    {
+        final String SQL = "update Student set age = ? where id = ?";
+        jdbcTemplateObject.update(new PreparedStatementCreator()
+        {
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException
+            {
+                PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+                preparedStatement.setInt(1, id);
+                preparedStatement.setInt(2, age);
+
+                return preparedStatement;
+            }
+        });
+        System.out.println("Updated Record with ID = " + id);
+        return;
+    }
+
+}
---
+++ src/main/java/org/mycompany/entity/mapper/StudentMapper.java
@@ -0,0 +1,20 @@
+package org.mycompany.entity.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.mycompany.entity.model.Student;
+import org.springframework.jdbc.core.RowMapper;
+
+public class StudentMapper implements RowMapper
+{
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException
+    {
+        Student student = new Student();
+        student.setId(rs.getInt("id"));
+        student.setName(rs.getString("name"));
+        student.setAge(rs.getInt("age"));
+        return student;
+    }
+
+}
